f55c41925e91ab283448a53d872eaf01eeeb706d
Draylon Vieira <draylid@gmail.com>
2023-01-19 20:00:58 -0300

    update release

--- lib/api/call.dart
+++ lib/api/call.dart
@@ -3,7 +3,7 @@ import 'package:flutter/foundation.dart' as Foundation;
 import 'package:http/http.dart' as http;
 
 class ApiRequests{
-    static const bool doSecure = false;
+    static const bool doSecure = true;
     static Future<http.Response> call(String route, [Map<String,dynamic>?queryParameters,]){
         if(!doSecure){
             print("Requesting on debug");
@@ -18,7 +18,7 @@ class ApiRequests{
         }else{
             print("Requesting on debug");
             return http.get(Uri.https(
-                "192.168.3.98:8080",route,queryParameters
+                "192.168.3.121:8080",route,queryParameters
             ));
         }
     }
